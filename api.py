57b3e47515df917ca0c992117892794adc068220
Николай Толстов <tolstovnv@gmail.com>
2021-04-16 15:46:49 +0800

    2 models trained on 2020 added

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from fastapi import Body, FastAPI, HTTPException
-from core.catboost_v1 import Catboost_v1_0, Catboost_v1_1, Catboost_v1_2
+from core.catboost_v1 import Catboost_v1_0, Catboost_v1_1, Catboost_v1_2, Catboost_v1_1_2020, Catboost_v1_2_2020
 import pandas as pd
 import ast
 from datetime import date
@@ -9,6 +9,8 @@ app = FastAPI()
 Catboost_model_v1_0 = Catboost_v1_0()
 Catboost_model_v1_1 = Catboost_v1_1()
 Catboost_model_v1_2 = Catboost_v1_2()
+Catboost_model_v1_1_2020 = Catboost_v1_1_2020()
+Catboost_model_v1_2_2020 = Catboost_v1_2_2020()
 
 # Check if fighter ID is in the base
 fighters_df = pd.read_csv("./Notebooks/Catboost_v1_0/data_models/fighters_df.csv", index_col=0)
@@ -54,6 +56,18 @@ def predict_fight(
                                                          f1_odd=f1_odd, f2_odd=f2_odd,
                                                          weightCategory_id=weightCategory_id, city=city, country=country,
                                                          event_name=event_name, time_zone=time_zone)
+
+        response_1_1_2020 = Catboost_model_v1_1_2020.predict_fight(f1_id=f1_id, f2_id=f2_id, event_date=event_date,
+                                                         f1_odd=f1_odd, f2_odd=f2_odd,
+                                                         weightCategory_id=weightCategory_id, city=city, country=country,
+                                                         event_name=event_name, time_zone=time_zone)
+
+        response_1_2_2020 = Catboost_model_v1_2_2020.predict_fight(f1_id=f1_id, f2_id=f2_id, event_date=event_date,
+                                                         f1_odd=f1_odd, f2_odd=f2_odd,
+                                                         weightCategory_id=weightCategory_id, city=city, country=country,
+                                                         event_name=event_name, time_zone=time_zone)
+
+
     except Exception as exp:
         output['success'] = False
         output['exp'] = exp
@@ -61,10 +75,12 @@ def predict_fight(
 
 
     output['y_proba_catboost_v1_0'] = response_1_0[0]
-    output['values']  = str(response_1_0[1])
-    output['columns'] = str(response_1_0[2])
+    # output['values']  = str(response_1_0[1])
+    # output['columns'] = str(response_1_0[2])
     output['y_proba_catboost_v1_1'] = response_1_1
     output['y_proba_catboost_v1_2'] = response_1_2
+    output['y_proba_catboost_v1_1_2020'] = response_1_1_2020
+    output['y_proba_catboost_v1_2_2020'] = response_1_2_2020
     # output = {'y_proba_catboost_v1_0': response_1_0[:1][0], 'X_df_values': list(response[1]),
     #           'X_df_columns': list(response[2])}
 
